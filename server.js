36e2e5fa1635ed69f7a8530d50fb85e9dc6e92ae
Pedro Souza <pedro.alcarin@gmail.com>
2022-10-22 02:13:33 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
   }) 
-app.get('/api/images', async (req, res) => {
+app.get('https://cloudinary-api-crud.herokuapp.com/api/images', async (req, res) => {
     const { resources } = await cloudinary.search
         .expression(`folder:${process.env.CLOUDINARY_FOLDER_NAME}`)
         .sort_by('public_id', 'desc')
