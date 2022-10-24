2564affa471fc60717d9034ea4e9c3fe7b8bfabc
Pedro Souza <pedro.alcarin@gmail.com>
2022-10-24 17:34:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ app.use(express.static('public'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors());
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*")
-  }) 
+  }) */
 app.get('/api/images', async (req, res) => {
     const { resources } = await cloudinary.search
         .expression(`folder:${process.env.CLOUDINARY_FOLDER_NAME}`)
